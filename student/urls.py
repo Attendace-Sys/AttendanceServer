12fa30cc70f83175fe667a773fed6fc085510df0
nguyentanphatka <nguyentanphatuit@gmail.com>
2019-12-04 11:14:13 +0700

    fix url

--- student/urls.py
+++ student/urls.py
@@ -4,13 +4,13 @@ from rest_framework import routers
 from django.conf.urls import url
 
 router = routers.DefaultRouter()
-router.register('students', views.StudentSerializerView, basename='serializers')
-router.register('images', views.StudentImagesDataView, basename='images')
+router.register('students/', views.StudentSerializerView, basename='serializers')
+router.register('images/', views.StudentImagesDataView, basename='images')
 
 urlpatterns=router.urls
 
 
 urlpatterns = [
-    path('serializer', include(router.urls)),
+    path('serializer/', include(router.urls)),
     url(r'^$', views.StudentView.as_view()),
 ]
