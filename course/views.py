38da1fbe67145bcc26e52b1d968e8dbacf10eb28
nguyentanphatka <nguyentanphatuit@gmail.com>
2020-01-07 19:23:13 +0700

    update model face landmarks

--- course/views.py
+++ course/views.py
@@ -365,6 +365,46 @@ class ListStudentOfCourseViewAPI(APIView):
 
 # API Get data  And do Face Recognition
 from io import BytesIO
+import time
+import os
+import PIL.Image
+import dlib
+import numpy as np
+from face_recognition.api import _raw_face_landmarks
+
+try:
+    import face_recognition_models
+except Exception:
+    print("Please install `face_recognition_models` with this command before using `face_recognition`:\n")
+    print("pip install git+https://github.com/ageitgey/face_recognition_models")
+    quit()
+face_detector = dlib.get_frontal_face_detector()
+
+predictor_68_point_model = face_recognition_models.pose_predictor_model_location()
+pose_predictor_68_point = dlib.shape_predictor(predictor_68_point_model)
+
+predictor_5_point_model = face_recognition_models.pose_predictor_five_point_model_location()
+pose_predictor_5_point = dlib.shape_predictor(predictor_5_point_model)
+
+cnn_face_detection_model = face_recognition_models.cnn_face_detector_model_location()
+cnn_face_detector = dlib.cnn_face_detection_model_v1(cnn_face_detection_model)
+
+face_recognition_model = face_recognition_models.face_recognition_model_location()
+face_encoder = dlib.face_recognition_model_v1(face_recognition_model)
+
+
+def face_encodings(face_image, known_face_locations=None, num_jitters=1):
+    """
+    Given an image, return the 128-dimension face encoding for each face in the image.
+
+    :param face_image: The image that contains one or more faces
+    :param known_face_locations: Optional - the bounding boxes of each face if you already know them.
+    :param num_jitters: How many times to re-sample the face when calculating encoding. Higher is more accurate, but slower (i.e. 100 is 100x slower)
+    :return: A list of 128-dimensional face encodings (one for each face in the image)
+    """
+    raw_landmarks = _raw_face_landmarks(face_image, known_face_locations, model="small")
+    return [np.array(face_encoder.compute_face_descriptor(face_image, raw_landmark_set, num_jitters)) for
+            raw_landmark_set in raw_landmarks]
 
 
 def get_list_of_list_face_vector_in_class_imgs(in_memory_uploaded_file_data, m_json_data):
@@ -398,7 +438,7 @@ def get_list_of_list_face_vector_in_class_imgs(in_memory_uploaded_file_data, m_j
             restored_img = Image.open(temp_file)
             restored_img = np.array(restored_img)
             try:
-                img_vector = face_recognition.face_encodings(restored_img, known_face_locations=[(0, 160, 160, 0)])[0]
+                img_vector = face_encodings(restored_img, known_face_locations=[(0, 160, 160, 0)])[0]
             except IndexError:
                 img_vector = np.zeros(128)
                 pass
@@ -413,10 +453,6 @@ def get_list_of_list_face_vector_in_class_imgs(in_memory_uploaded_file_data, m_j
 def preprocess_image(image_path):
     img = load_img(image_path, target_size=(160, 160))
     return np.array(img)
-
-
-import time
-import os
 
 
 # load images and extract faces for all images in a directory
@@ -447,10 +483,9 @@ def load_faces(directory):
 
             try:
                 # (top, right, bottom, left)
-                img_vector = face_recognition.face_encodings(img, known_face_locations=[(0, 160, 160, 0)])[0]
+                img_vector = face_encodings(img, known_face_locations=[(0, 160, 160, 0)])[0]
                 # ---> array [[128-d vector for face 1], [128-d vector for face 2], ....]
                 # print("extract 1 img vector--- %s seconds ---" % (time.time() - start_time))
-
                 # save img_vector to use later
                 np.save(numpy_vector_path, img_vector)
 
